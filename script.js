6ebce842ca40337543068b24591f9ebaaa669a36
ernmin <42770608+ernmin@users.noreply.github.com>
2024-09-01 01:33:32 +0800

    Change to use Grid Create container base with Grid instead of Flexbox

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
-
-    const row = document.createElement("div")
     
-    function createRow(rowlength){
-        row.classList.add("rowbox");
-        container.appendChild(row);
-        for(let i = 0; i < rowlength; i++){
-            const box = document.createElement("div");
-            box.classList.add("box");
-            box.style.border = "thin solid black";
-            row.appendChild(box);
-        }
-    }
-    const container = document.querySelector(".container");
-    createRow(3)
-
-
-
-
-
-
-
 
 }
 )
