5c91ece78aa7e25390be6e6590991193830937b7
ernmin <42770608+ernmin@users.noreply.github.com>
2024-09-20 13:18:42 +0800

    Fill all squares with X or O Change game array to a 2D array to keep track of what has been clicked Add event listener for all cells Change boxes to be labelled 0 to 8 instead of 1 to 9

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 let arr = []
-for(let i = 0; i < 9; i++){
-    arr[i] = null;
+const row = 9
+const column = 9
+for(let i = 0; i < row; i++){
+    arr[i] = [];
+    for(let y = 0; y < column; y++){
+        arr[i][y] = null;
+    }
 }
 
 let turn_count = 0;
@@ -13,11 +18,14 @@ null is empty
 1 is cross
 */
 
-function box1(){
-    let box1 = document.querySelector('#box-5-1');
-    let box_id = 1;
+
+function box(game_num, box_num){
+    /*let box_num = 1;*/
+    let selector = '#box' + '-' + game_num + '-' + box_num;
+    let box1 = document.querySelector(selector);
+    /*let box_id = 1;*/
     box1.addEventListener('click', function(){
-        if (arr[box_id] != null){
+        if (arr[game_num][box_num] != null){
             return;
         }
         else {
@@ -25,15 +33,17 @@ function box1(){
             if (turn_count % 2 == 0){
                 image.src = "o-mark.png"
                 box1.appendChild(image);
-                arr[box_id] = 0;
+                arr[game_num][box_num] = 0;
                 turn_count += 1;
+                console.log(turn_count);
             }
             else {
                 
                 image.src = "x-mark.png"
                 box1.appendChild(image);
-                arr[box_id] = 1;
+                arr[game_num][box_num] = 1;
                 turn_count += 1;
+                console.log(turn_count);
             }
         }
     });
@@ -42,8 +52,11 @@ function box1(){
 function reset(){
     let reset = document.querySelector("#reset");
     reset.addEventListener('click', function(){
-        for(let i = 0; i < 9; i++){
-            arr[i] = null;
+        for(let i = 0; i < row; i++){
+            arr[i] = [];
+            for(let y = 0; y < column; y++){
+                arr[i][y] = null;
+            }
         }
         turn_count = 0;
         let all_cells = document.querySelectorAll(".item");
@@ -58,8 +71,11 @@ function reset(){
     
 }
 
-box1();
-box2();
+for (let box_num = 0; box_num < 9; box_num++){
+    for (let box_sub_num = 0; box_sub_num < 9; box_sub_num++)
+    box(box_num, box_sub_num);
+}
+
 reset();
 
 
