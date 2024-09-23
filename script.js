2763b8004ab9fbc72f49ae29fee01807e91604bc
ernmin <42770608+ernmin@users.noreply.github.com>
2024-09-23 12:37:54 +0800

    Indicate current player's turn Include new div with id = whose-turn and add text to indicate current player's turn. Create new function based on the current turn count to indicate this.

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 let arr = []
-const row = 9
-const column = 9
-for(let i = 0; i < row; i++){
+const game_num = 9
+const box_num = 9
+for(let i = 0; i < game_num; i++){
     arr[i] = [];
-    for(let y = 0; y < column; y++){
+    for(let y = 0; y < box_num; y++){
         arr[i][y] = null;
     }
 }
 
 let turn_count = 0;
+const turn = document.querySelector('#whose-turn');
 
 /*
 null is empty
 0 is circle
 1 is cross
-*/
+*/ 
 
 
 function box(game_num, box_num){
@@ -34,16 +35,18 @@ function box(game_num, box_num){
                 image.src = "o-mark.png"
                 box1.appendChild(image);
                 arr[game_num][box_num] = 0;
+                console.log('game num is', game_num, 'box num is', box_num);
                 turn_count += 1;
-                console.log(turn_count);
+                indicate_turn();
             }
             else {
                 
                 image.src = "x-mark.png"
                 box1.appendChild(image);
                 arr[game_num][box_num] = 1;
+                console.log('game num is', game_num, 'box num is', box_num);
                 turn_count += 1;
-                console.log(turn_count);
+                indicate_turn();
             }
         }
     });
@@ -52,13 +55,14 @@ function box(game_num, box_num){
 function reset(){
     let reset = document.querySelector("#reset");
     reset.addEventListener('click', function(){
-        for(let i = 0; i < row; i++){
+        for(let i = 0; i < game_num; i++){
             arr[i] = [];
-            for(let y = 0; y < column; y++){
+            for(let y = 0; y < box_num; y++){
                 arr[i][y] = null;
             }
         }
         turn_count = 0;
+        indicate_turn();
         let all_cells = document.querySelectorAll(".item");
         for (let i = 0; i < all_cells.length; i++){
             if (all_cells[i].hasChildNodes()){
@@ -71,11 +75,22 @@ function reset(){
     
 }
 
+function indicate_turn(){
+    
+    if (turn_count % 2 == 0){
+        turn.textContent = "It is circle's turn";
+    }
+    else {
+        turn.textContent = "It is cross' turn";
+    }
+    
+}
+
 for (let box_num = 0; box_num < 9; box_num++){
     for (let box_sub_num = 0; box_sub_num < 9; box_sub_num++)
     box(box_num, box_sub_num);
 }
-
+indicate_turn();
 reset();
 
 
