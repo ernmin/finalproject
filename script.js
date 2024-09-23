0a7413f704a103a64ec161e21bc1eaea90e074e5
ernmin <42770608+ernmin@users.noreply.github.com>
2024-09-23 13:22:45 +0800

    Implement check small game function Use 1 main function and 3 sub functions to check for the win condition of a small game.

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ function box(game_num, box_num){
                 box1.appendChild(image);
                 arr[game_num][box_num] = 0;
                 console.log('game num is', game_num, 'box num is', box_num);
+                console.log(check_small_game(game_num));
                 turn_count += 1;
                 indicate_turn();
             }
@@ -45,6 +46,7 @@ function box(game_num, box_num){
                 box1.appendChild(image);
                 arr[game_num][box_num] = 1;
                 console.log('game num is', game_num, 'box num is', box_num);
+                console.log(check_small_game(game_num));
                 turn_count += 1;
                 indicate_turn();
             }
@@ -62,7 +64,7 @@ function reset(){
             }
         }
         turn_count = 0;
-        indicate_turn();
+        indicate_turn(); 
         let all_cells = document.querySelectorAll(".item");
         for (let i = 0; i < all_cells.length; i++){
             if (all_cells[i].hasChildNodes()){
@@ -76,14 +78,77 @@ function reset(){
 }
 
 function indicate_turn(){
-    
     if (turn_count % 2 == 0){
         turn.textContent = "It is circle's turn";
     }
     else {
         turn.textContent = "It is cross' turn";
     }
-    
+}
+
+function check_small_game(game_num){
+    if (check_row(game_num) == 1){
+        return 1;
+    }
+    else if(check_column(game_num) == 1){
+        return 1;
+    }
+    else if(check_diagonal(game_num) == 1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+function check_row(game_num){
+    for(let cell_row = 0; cell_row < 7; cell_row = cell_row + 3){
+        if (arr[game_num][cell_row] != null){
+            if (arr[game_num][cell_row] == arr[game_num][cell_row + 1] && arr[game_num][cell_row + 1] == arr[game_num][cell_row + 2]){
+                return 1;
+            }
+            else {
+                continue;
+            }
+        }
+        else {
+            continue;
+        }
+    }
+    return 0;
+}
+
+function check_column(game_num){
+    for(let cell_column = 0; cell_column < 3; cell_column++){
+        if (arr[game_num][cell_column] != null){
+            if (arr[game_num][cell_column] == arr[game_num][cell_column + 3] && arr[game_num][cell_column + 3] == arr[game_num][cell_column + 6]){
+                return 1;
+            }
+            else {
+                continue;
+            }
+        }
+        else {
+            continue;
+        }
+    }
+    return 0;
+}
+
+function check_diagonal(game_num){
+    let cell_diagonal = 4;
+    if (arr[game_num][cell_diagonal] == null){
+        return 0;
+    }
+    else if (arr[game_num][cell_diagonal] == arr[game_num][cell_diagonal - 2] && arr[game_num][cell_diagonal] == arr[game_num][cell_diagonal + 2]){
+        return 1;
+    }
+    else if (arr[game_num][cell_diagonal] == arr[game_num][cell_diagonal - 4] && arr[game_num][cell_diagonal] == arr[game_num][cell_diagonal + 4]){
+        return 1;
+    }
+    else {
+        return 0;
+    }
 }
 
 for (let box_num = 0; box_num < 9; box_num++){
@@ -98,6 +163,7 @@ reset();
 1. implement reset function
 2. Use function for all the boxes in the game
 3. implement check function after each move
+4. if there is a win, check who won using the turn count
 */
 
 }
